2e930c9ee12b578319c558247409b54672d9c2e6
rizwanishaq <rizwanishaq@gmail.com>
2019-11-22 11:49:57 +0100

    first commit

---
+++ src/user/pages/Users.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "what-ever",
+      image:
+        "https://images.pexels.com/photos/2755165/pexels-photo-2755165.jpeg?cs=srgb&dl=photo-of-woman-wearing-yellow-jacket-2755165.jpg&fm=jpg",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
